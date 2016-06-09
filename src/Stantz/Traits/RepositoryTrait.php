6105f3151d0d27756cbb8cf523ba113fc6fc17bb
Lark Mullins <larkmullins@gmail.com>
2016-06-09 16:49:34 -0400

    Added Traits

---
+++ src/Stantz/Traits/RepositoryTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * RepositoryTrait
+ *
+ * Trait for Table classes
+ *
+ * @author      Stantz Development
+ * @package     Stantz\Traits
+ * @version     0.1.0
+ *
+ */
+
+namespace Stantz\Traits;
+
+trait RepositoryTrait
+{
+    private $persistence;
+}
